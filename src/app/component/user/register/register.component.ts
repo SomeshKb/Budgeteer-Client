59ef23070291a24d73251e86e76edaf1e7c316ce
Somesh Bhaskar <somesh.bhaskar@diasparkindia.com>
2019-01-18 19:53:30 +0530

    added proxy changes

--- src/app/component/user/register/register.component.ts
+++ src/app/component/user/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AlertService } from 'src/app/service/shared/alert.service';
+import { AuthenticationService } from 'src/app/service/user/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+  loading = false;
 
-  constructor() { }
+  constructor(private alertService: AlertService, private auth: AuthenticationService,
+    private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    if (this.auth.isLoggedIn()) {
+      this.router.navigateByUrl('/events');
+    }
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    } else {
+      this.register(this.registerForm.value);
+    }
+  }
+
+  register(credentials: any) {
+    this.auth.register(credentials).subscribe((res) => {
+      this.router.navigateByUrl('/home');
+    }, (err) => {
+      console.log(err);
+      if (err.status === 409) {
+        this.alertService.error('User Already Exists !!');
+      }
+    });
+  }
+
+
+
 
 }
