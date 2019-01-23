fe2d73cc509793922b421a40e64a0066b6483533
Somesh Bhaskar <somesh.bhaskar@diasparkindia.com>
2019-01-23 18:48:02 +0530

    chamged app name

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Budgeteer-Client'));
+app.use(express.static(__dirname + '/dist/Budgeteer'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/Budgeteer-Client/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Budgeteer/index.html'));
 });
 
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Budgeteer-Client'`, async(() => {
+  it(`should have as title 'Budgeteer'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Budgeteer-Client');
+    expect(app.title).toEqual('Budgeteer');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Budgeteer-Client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Budgeteer!');
   }));
 });
